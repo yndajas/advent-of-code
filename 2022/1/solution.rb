6554a25c666e3010a232ef8145ea2fe337b17280
Ynda Jas <yndajas@gmail.com>
2022-12-03 17:24:28 +0000

    Add 2022 day one, part one
    This includes the descriptuon, input and solution for part one of
    2022 day one

---
+++ 2022/1/solution.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+filename = ARGV.first
+calories_by_elf = []
+elf_index = 0
+
+File.readlines(filename).each do |line|
+  if line == "\n"
+    elf_index += 1
+    next
+  end
+
+  current_calories = calories_by_elf[elf_index] || 0
+  calories_to_add = line.to_i
+
+  calories_by_elf[elf_index] = current_calories + calories_to_add
+end
+
+puts calories_by_elf.max
