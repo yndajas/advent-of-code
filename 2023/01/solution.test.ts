ab5636c5211ce23c7a9d35ccb95a843d43504039
Ynda Jas <yndajas@gmail.com>
2023-12-01 22:16:59 +0000

    Add ESLint

--- 2023/01/solution.test.ts
+++ 2023/01/solution.test.ts
@@ -1,17 +1,17 @@
-import { describe, expect, it } from "bun:test";
+import { describe, expect, it } from 'bun:test'
 
-import { partOne, partTwo } from "./solution";
+import { partOne, partTwo } from './solution'
 
-describe("2023 day one", () => {
-  describe("part one", () => {
-    it("returns 54877", async () => {
-      expect(await partOne()).toEqual(54877);
-    });
-  });
+describe('2023 day one', () => {
+  describe('part one', () => {
+    it('returns 54877', async () => {
+      expect(await partOne()).toEqual(54877)
+    })
+  })
 
-  describe("part two", () => {
-    it("returns 54100", async () => {
-      expect(await partTwo()).toEqual(54100);
-    });
-  });
-});
+  describe('part two', () => {
+    it('returns 54100', async () => {
+      expect(await partTwo()).toEqual(54100)
+    })
+  })
+})
--- 2023/01/solution.ts
+++ 2023/01/solution.ts
@@ -1,90 +1,71 @@
 const getInput = async (): Promise<string> => {
-  return Bun.file("./2023/01/input").text();
-};
+  return Bun.file('./2023/01/input').text()
+}
 
 const partOne = async (): Promise<number> => {
-  const input = await getInput();
+  const input = await getInput()
 
-  const lines = input.split("\n");
+  const lines = input.split('\n')
   const result = lines.reduce((accumulator, line) => {
-    const digits = line.match(/[0-9]/g) as Array<string>;
-    const number = Number(digits[0] + digits[digits.length - 1]);
-    return accumulator + number;
-  }, 0);
+    const digits = line.match(/[0-9]/g) as Array<string>
+    const number = Number(digits[0] + digits[digits.length - 1])
+    return accumulator + number
+  }, 0)
 
-  return result;
-};
+  return result
+}
 
-type Digit = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
-type DigitWord =
-  | "one"
-  | "two"
-  | "three"
-  | "four"
-  | "five"
-  | "six"
-  | "seven"
-  | "eight"
-  | "nine";
+type Digit = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+type DigitWord = 'eight' | 'five' | 'four' | 'nine' | 'one' | 'seven' | 'six' | 'three' | 'two'
 
-const isDigit = (
-  digitOrDigitWord: Digit | DigitWord
-): digitOrDigitWord is Digit => {
-  return digitOrDigitWord.length === 1;
-};
+const isDigit = (digitOrDigitWord: Digit | DigitWord): digitOrDigitWord is Digit => {
+  return digitOrDigitWord.length === 1
+}
 
-const digitOrDigitWordToDigit = (
-  digitOrDigitWord: Digit | DigitWord
-): Digit => {
+const digitOrDigitWordToDigit = (digitOrDigitWord: Digit | DigitWord): Digit => {
   if (isDigit(digitOrDigitWord)) {
-    return digitOrDigitWord;
+    return digitOrDigitWord
   }
 
-  const digitWordToDigit: { [key: string]: Digit } = {
-    one: "1",
-    two: "2",
-    three: "3",
-    four: "4",
-    five: "5",
-    six: "6",
-    seven: "7",
-    eight: "8",
-    nine: "9",
-  };
+  const digitWordToDigit: Record<string, Digit> = {
+    eight: '8',
+    five: '5',
+    four: '4',
+    nine: '9',
+    one: '1',
+    seven: '7',
+    six: '6',
+    three: '3',
+    two: '2',
+  }
 
-  return digitWordToDigit[digitOrDigitWord];
-};
+  return digitWordToDigit[digitOrDigitWord]
+}
 
 const partTwo = async (): Promise<number> => {
-  const input = await getInput();
-  const lines = input.split("\n");
+  const input = await getInput()
+  const lines = input.split('\n')
   const result = lines.reduce((accumulator, line) => {
-    const digitsAndDigitWords: Array<Digit | DigitWord> = [];
-    const regex = /^([0-9]|one|two|three|four|five|six|seven|eight|nine)/;
+    const digitsAndDigitWords: Array<Digit | DigitWord> = []
+    const regex = /^([0-9]|one|two|three|four|five|six|seven|eight|nine)/
 
-    for (
-      let characterIndex = 0;
-      characterIndex < line.length;
-      characterIndex++
-    ) {
-      const matchArray = line.substring(characterIndex).match(regex);
+    for (let characterIndex = 0; characterIndex < line.length; characterIndex++) {
+      const matchArray = line.substring(characterIndex).match(regex)
 
       if (matchArray) {
-        digitsAndDigitWords.push(matchArray[0] as Digit | DigitWord);
+        digitsAndDigitWords.push(matchArray[0] as Digit | DigitWord)
       }
     }
 
     const number = Number(
       digitOrDigitWordToDigit(digitsAndDigitWords[0]) +
-        digitOrDigitWordToDigit(
-          digitsAndDigitWords[digitsAndDigitWords.length - 1]
-        )
-    );
+        digitOrDigitWordToDigit(digitsAndDigitWords[digitsAndDigitWords.length - 1]),
+    )
 
-    return accumulator + number;
-  }, 0);
+    return accumulator + number
+  }, 0)
 
-  return result;
-};
+  return result
+}
 
-export { partOne, partTwo };
+export { partOne, partTwo }
