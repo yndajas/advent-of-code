c3a7bc1d65e9dbd6bd371732b6b7efd79afa7117
Ynda Jas <yndajas@gmail.com>
2023-12-05 23:16:20 +0000

    Add 2023 day 4 part 2

--- 2023/04/solution.test.ts
+++ 2023/04/solution.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it } from 'bun:test'
 
-import { partOne } from './solution'
+import { partOne, partTwo } from './solution'
 
 describe('2023 day 4', () => {
   describe('part 1', () => {
     it('returns 24848', async () => {
       expect(await partOne()).toEqual(24848)
+    })
+  })
+
+  describe('part 2', () => {
+    it('returns 7258152', async () => {
+      expect(await partTwo()).toEqual(7258152)
     })
   })
 })
--- 2023/04/solution.ts
+++ 2023/04/solution.ts
@@ -2,27 +2,46 @@ import InputUtils from '../inputUtils'
 
 const inputPath = './2023/04/input'
 
+const countWinningNumbersYouHave = (line: string): number => {
+  const numbersStartingIndex = line.indexOf(':') + 2
+  const [winningNumbersString, numbersYouHaveString] = line.substring(numbersStartingIndex).split('|')
+  const winningNumbers = winningNumbersString.match(/[0-9]+/g) as RegExpMatchArray
+  const numbersYouHave = numbersYouHaveString.match(/[0-9]+/g) as RegExpMatchArray
+
+  return numbersYouHave.reduce(
+    (winningNumbersYouHaveCountAccumulator, numberYouHave) =>
+      winningNumbers.includes(numberYouHave)
+        ? winningNumbersYouHaveCountAccumulator + 1
+        : winningNumbersYouHaveCountAccumulator,
+    0,
+  )
+}
+
 const partOne = async (): Promise<number> => {
   const lines = await InputUtils.getInputLines(inputPath)
 
   return lines.reduce((pointsAccumulator, line) => {
-    const numbersStartingIndex = line.indexOf(':') + 2
-    const [winningNumbersString, numbersYouHaveString] = line.substring(numbersStartingIndex).split('|')
-    const winningNumbers = winningNumbersString.match(/[0-9]+/g) as RegExpMatchArray
-    const numbersYouHave = numbersYouHaveString.match(/[0-9]+/g) as RegExpMatchArray
-
-    const winningNumbersYouHaveCount = numbersYouHave.reduce(
-      (winningNumbersYouHaveCountAccumulator, numberYouHave) =>
-        winningNumbers.includes(numberYouHave)
-          ? winningNumbersYouHaveCountAccumulator + 1
-          : winningNumbersYouHaveCountAccumulator,
-      0,
-    )
-
+    const winningNumbersYouHaveCount = countWinningNumbersYouHave(line)
     const pointsForLine = winningNumbersYouHaveCount ? 2 ** (winningNumbersYouHaveCount - 1) || 1 : 0
 
     return pointsAccumulator + pointsForLine
   }, 0)
 }
 
-export { partOne }
+const partTwo = async (): Promise<number> => {
+  const lines = await InputUtils.getInputLines(inputPath)
+  const lineDetails = lines.map(line => ({
+    count: 1,
+    winningNumbersYouHave: countWinningNumbersYouHave(line),
+  }))
+
+  lineDetails.forEach((lineDetail, lineDetailIndex) => {
+    for (let lineOffset = 1; lineOffset <= lineDetail.winningNumbersYouHave; lineOffset++) {
+      lineDetails[lineDetailIndex + lineOffset].count += lineDetail.count
+    }
+  })
+
+  return lineDetails.reduce((accumulator, lineDetail) => accumulator + lineDetail.count, 0)
+}
+
+export { partOne, partTwo }
