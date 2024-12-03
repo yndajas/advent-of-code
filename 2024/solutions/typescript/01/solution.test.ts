a2e60dce48e799f8bdae56031f6cdc5bd0856ba9
Ynda Jas <yndajas@gmail.com>
2024-12-03 03:42:49 +0000

    Fix 2024/01 TypeScript test description

--- 2024/solutions/typescript/01/solution.test.ts
+++ 2024/solutions/typescript/01/solution.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "bun:test";
 
 import { partOne, partTwoSolutionOne, partTwoSolutionTwo } from "./solution";
 
-describe("2023 day 5", () => {
+describe("2024 day 1", () => {
 	describe("part 1", () => {
 		it("returns 1189304", async () => {
 			expect(await partOne()).toEqual(1189304);
