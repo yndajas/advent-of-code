51d33246d94947449176afdaefe72cc198c1de4b
Ynda Jas <yndajas@gmail.com>
2024-12-05 10:52:21 +0000

    Add 2024/04 part 2 TypeScript solution

--- 2024/solutions/typescript/05/solution.test.ts
+++ 2024/solutions/typescript/05/solution.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it } from "bun:test";
 
-import { partOne } from "./solution";
+import { partOne, partTwo } from "./solution";
 
 describe("2024 day 5", () => {
 	describe("part 1", () => {
 		it("returns 4905", async () => {
 			expect(partOne()).toEqual(4905);
+		});
+	});
+
+	describe("part 2", () => {
+		it("returns 6204", async () => {
+			expect(partTwo()).toEqual(6204);
 		});
 	});
 });
--- 2024/solutions/typescript/05/solution.ts
+++ 2024/solutions/typescript/05/solution.ts
@@ -4,41 +4,28 @@ const [rules, updates] = (
 	.split("\n\n")
 	.map((section) => section.split("\n"));
 
+type PrecedingNumbers = number[];
+
+const precedingNumbersByNumber: Record<number, PrecedingNumbers> = {};
+
+for (const rule of rules) {
+	const [precedingNumber, number] = rule
+		.split("|")
+		.map((numberString) => Number.parseInt(numberString, 10));
+
+	precedingNumbersByNumber[number] = (
+		precedingNumbersByNumber[number] || []
+	).concat(precedingNumber);
+}
+
 function partOne() {
-	type PrecedingNumbers = number[];
-	const precedingNumbersByNumber: Record<number, PrecedingNumbers> = {};
-
-	for (const rule of rules) {
-		const [precedingNumber, number] = rule
-			.split("|")
-			.map((numberString) => Number.parseInt(numberString, 10));
-
-		precedingNumbersByNumber[number] = (
-			precedingNumbersByNumber[number] || []
-		).concat(precedingNumber);
-	}
-
 	return updates
 		.map((update): number | undefined => {
 			const numbers = update
 				.split(",")
 				.map((numberString) => Number.parseInt(numberString, 10));
 
-			let correctOrder = true;
-
-			for (
-				let numberIndex = 0;
-				numberIndex < numbers.length - 1 && correctOrder;
-				numberIndex++
-			) {
-				const currentNumber = numbers[numberIndex];
-				const nextNumber = numbers[numberIndex + 1];
-				correctOrder =
-					!precedingNumbersByNumber[currentNumber] ||
-					!precedingNumbersByNumber[currentNumber].includes(nextNumber);
-			}
-
-			if (correctOrder) {
+			if (isCorrectlyOrdered(numbers)) {
 				const middleIndex = Math.floor(numbers.length / 2);
 				return numbers[middleIndex];
 			}
@@ -53,6 +40,66 @@ function partOne() {
 		);
 }
 
-console.log(partOne());
+function partTwo() {
+	return updates
+		.map((update): number | undefined => {
+			const numbers = update
+				.split(",")
+				.map((numberString) => Number.parseInt(numberString, 10));
 
-export { partOne };
+			if (!isCorrectlyOrdered(numbers)) {
+				const middleIndex = Math.floor(numbers.length / 2);
+				return fixOrder(numbers)[middleIndex];
+			}
+		})
+		.filter(
+			(middleNumberFromCorrectlyOrderedUpdateOrUndefined) =>
+				middleNumberFromCorrectlyOrderedUpdateOrUndefined,
+		)
+		.reduce(
+			(accumulator, currentMiddleNumber) => accumulator + currentMiddleNumber,
+			0,
+		);
+}
+
+function isCorrectlyOrdered(numbers: number[]) {
+	let correctOrder = true;
+
+	for (
+		let numberIndex = 0;
+		numberIndex < numbers.length - 1 && correctOrder;
+		numberIndex++
+	) {
+		const currentNumber = numbers[numberIndex];
+		const nextNumber = numbers[numberIndex + 1];
+		correctOrder =
+			!precedingNumbersByNumber[currentNumber] ||
+			!precedingNumbersByNumber[currentNumber].includes(nextNumber);
+	}
+
+	return correctOrder;
+}
+
+function fixOrder(numbers: number[]) {
+	for (let numberIndex = 0; numberIndex < numbers.length - 1; numberIndex++) {
+		const currentNumber = numbers[numberIndex];
+		const nextNumber = numbers[numberIndex + 1];
+
+		if (precedingNumbersByNumber[currentNumber]?.includes(nextNumber)) {
+			const nextArray = [
+				...numbers.slice(0, numberIndex),
+				nextNumber,
+				currentNumber,
+				...numbers.slice(numberIndex + 2),
+			];
+			return fixOrder(nextArray);
+		}
+	}
+
+	return numbers;
+}
+
+console.log(partOne());
+console.log(partTwo());
+
+export { partOne, partTwo };
