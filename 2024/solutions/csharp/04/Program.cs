b6b4446325fcddad3b68590933e16c1619491fd8
Ynda Jas <yndajas@gmail.com>
2024-12-08 03:56:53 +0000

    Add 2024/04.1 C#/.NET solution
    In noticed that I used lists instead of arrays in the day 1 C#/.NET
    solutions. In day two, `originalAndSubArrays` used arrays but only one
    was modified, and that was reassigned with the original array spread
    into a new array, instead of with pushing (via `Add` or `Append`). Day
    three didn't have any arrays or lists
    
    I encountered some challenges in growing arrays here, so I went back to
    using lists for this purpose, which seems to popular:
    https://stackoverflow.com/a/434765/4002016. I did however convert back
    to an array once the size is fixed. Not sure if this is advisable or
    whether there are performance gains from using an array or losses from
    the conversion
    
    C#/.NET will raise an out of bounds error if trying to access an array
    element that doesn't exist by index, so in the `while` loop in
    `rowsToColumns` the parenthetical current or default expression in
    `columns[currentColumnIndex] = (columns[currentColumnIndex] || "") +
    currentCharacter;` from the TypeScript solution won't work here. Instead
    I checked the index against the length and added the default if it would
    be out of bounds (before the statement that adds the current character).
    Perhaps there's a cleaner/more succinct way of doing this?

---
+++ 2024/solutions/csharp/04/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Text.RegularExpressions;
+
+string[] rows = File.ReadAllLines("../../../input/04");
+int maxRowIndex = rows.Length - 1;
+int maxColumnIndex = rows[0].Length - 1;
+
+int partOne()
+{
+  string[] columns = rowsToColumns(rows);
+  string[] diagonalLines = rowsToDiagonalLines(rows);
+  string[] allLines = [.. rows, .. columns, .. diagonalLines];
+  string regex = @"(?=(?:XMAS|SAMX))";
+
+  int totalMatchCount = 0;
+
+  foreach (string currentLine in allLines)
+  {
+    int currentLineMatchCount = Regex.Matches(currentLine, regex).Count;
+    totalMatchCount += currentLineMatchCount;
+  }
+
+  return totalMatchCount;
+}
+
+Console.WriteLine(partOne());
+
+string[] rowsToColumns(string[] rows)
+{
+  List<string> columns = [];
+
+  foreach (string currentRow in rows)
+  {
+    int currentColumnIndex = 0;
+
+    while (currentColumnIndex <= maxColumnIndex)
+    {
+      if (currentColumnIndex > columns.Count - 1)
+      {
+        columns.Add("");
+      }
+
+      char currentCharacter = currentRow[currentColumnIndex];
+      columns[currentColumnIndex] += currentCharacter;
+      currentColumnIndex++;
+    }
+  }
+
+  return [.. columns];
+}
+
+string[] rowsToDiagonalLines(string[] rows)
+{
+  List<string> diagonalLines = [];
+
+  for (int currentColumnIndex = 0; currentColumnIndex <= maxColumnIndex; currentColumnIndex++)
+  {
+    diagonalLines.Add(downRightDiagonalLineFromCell(0, currentColumnIndex));
+    diagonalLines.Add(downLeftDiagonalLineFromCell(0, currentColumnIndex));
+  }
+
+  for (int currentRowIndex = 1; currentRowIndex <= maxRowIndex; currentRowIndex++)
+  {
+    diagonalLines.Add(downRightDiagonalLineFromCell(currentRowIndex, 0));
+    diagonalLines.Add(downLeftDiagonalLineFromCell(currentRowIndex, maxColumnIndex));
+  }
+
+  return [.. diagonalLines];
+}
+
+string downRightDiagonalLineFromCell(int startingRowIndex, int startingColumnIndex)
+{
+  int currentRowIndex = startingRowIndex;
+  int currentColumnIndex = startingColumnIndex;
+  string lineString = "";
+
+  while (currentRowIndex <= maxRowIndex && currentColumnIndex <= maxColumnIndex)
+  {
+    lineString += rows[currentRowIndex][currentColumnIndex];
+    currentRowIndex++;
+    currentColumnIndex++;
+  }
+
+  return lineString;
+}
+
+string downLeftDiagonalLineFromCell(int startingRowIndex, int startingColumnIndex)
+{
+  int currentRowIndex = startingRowIndex;
+  int currentColumnIndex = startingColumnIndex;
+  string lineString = "";
+
+  while (currentRowIndex <= maxRowIndex && currentColumnIndex >= 0)
+  {
+    lineString += rows[currentRowIndex][currentColumnIndex];
+    currentRowIndex++;
+    currentColumnIndex--;
+  }
+
+  return lineString;
+}
