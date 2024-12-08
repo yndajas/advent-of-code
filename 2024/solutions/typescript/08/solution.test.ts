2ab4eacb3223ce10e05a156b87d5830f1d7083c4
Ynda Jas <yndajas@gmail.com>
2024-12-08 10:04:20 +0000

    Add 2024/08.2 TypeScript solution
    Unfortunately the description for this part was uncharacteristically
    confusing
    
    > After updating your model, it turns out that an antinode occurs at any
    > grid position exactly in line with at least two antennas of the same
    > frequency, regardless of distance.
    
    This makes it sound like you'll need to work out the smallest unit of
    steps between each antenna of the same frequency where x and y are both
    integers, then walk the line in those units counting all cells except
    the antennae unless there are three antennae on the line
    
    > This means that some of the new antinodes will occur at the position
    > of each antenna (unless that antenna is the only one of its
    > frequency).
    
    This then hints that having two antennae on a line is enough to count
    them, which is then suggested more heavily by the subsequent example
    (see the HTML)
    
    It seems that the latter is true, so all antennae and antinodes on a
    given (antennae-based) line count. But it also seems that you aren't
    meant to divide the diff between two antennae into its smallest integral
    (integer-based) subunits; either that or the data are such that it's
    unnecessary (this is definitely the case with the example, which only
    has indivisible diffs between each same-frequency antennae)
    
    As such, the change here isn't that substantial, and much less
    substantial than the description might have you think necessary: rather
    than going once in each direction, keep going until out of bounds

--- 2024/solutions/typescript/08/solution.test.ts
+++ 2024/solutions/typescript/08/solution.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it } from "bun:test";
 
-import { partOne } from "./solution";
+import { partOne, partTwo } from "./solution";
 
 describe("2024 day 8", () => {
 	describe("part 1", () => {
 		it("returns 291", async () => {
 			expect(partOne()).toEqual(291);
+		});
+	});
+
+	describe("part 2", () => {
+		it("returns 1015", async () => {
+			expect(partTwo()).toEqual(1015);
 		});
 	});
 });
