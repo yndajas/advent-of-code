78f70e40d1879e8d607f2d829581f1991963521a
Ynda Jas <yndajas@gmail.com>
2024-12-09 13:02:38 +0000

    Add 2024/09.2 TypeScript solution
    It would be nice to break down the function here, but the algorithm
    feels okay. It's very similar to part one, but just different enough to
    not feel like it warranted abstracting anything common out (making
    things similar enough might have actually reduced performance)
    
    It took me an unfortunate amount of time to debug an issue where my
    array was growing during the defragmentation stage. I was looking for
    suitable spaces for move candidates in the whole array, rather than just
    on the left side of a given move candidate
    
    Switching to `console.table` instead of `console.log` really helped with
    the debugging: seeing the index and all data of each file or empty space
    object on a single line was very useful
    
    Speaking of useful, so too was having the prompt browser in my terminal.
    I ended up side-by-siding the prompt and either logging output or a
    REPL, which allowed me to compared results of various steps with the
    examples and values in the prompt
    
    For both parts today, the general algorithm came quite naturally, and
    they seem to be pretty performant (~3ms(!) for part one and a little
    over 300ms for part two) but I made some pretty simple errors. Need more
    sleep

--- 2024/solutions/typescript/09/solution.test.ts
+++ 2024/solutions/typescript/09/solution.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it } from "bun:test";
 
-import { partOne } from "./solution";
+import { partOne, partTwo } from "./solution";
 
 describe("2024 day 9", () => {
 	describe("part 1", () => {
 		it("returns 6283404590840", async () => {
 			expect(partOne()).toEqual(6283404590840);
+		});
+	});
+
+	describe("part 2", () => {
+		it("returns 6304576012713", async () => {
+			expect(partTwo()).toEqual(6304576012713);
 		});
 	});
 });
--- 2024/solutions/typescript/09/solution.ts
+++ 2024/solutions/typescript/09/solution.ts
@@ -60,4 +60,115 @@ function checksum(blocks: number[]) {
 	);
 }
 
-export { partOne };
+type File = { id: number; length: number };
+type EmptySpace = { id: -1; length: number };
+type FileSystem = (File | EmptySpace)[];
+
+function partTwo() {
+	let fileSystem: FileSystem = [];
+
+	// map file system
+	for (
+		let diskMapIndex = 0;
+		diskMapIndex < diskMapString.length;
+		diskMapIndex++
+	) {
+		const length = Number.parseInt(diskMapString[diskMapIndex]);
+
+		if (isFile(diskMapIndex)) {
+			fileSystem.push({ id: fileId(diskMapIndex), length });
+		} else {
+			if (length === 0) {
+				continue;
+			}
+
+			fileSystem.push({ id: -1, length });
+		}
+	}
+
+	// defragment
+	for (let candidateIndex = fileSystem.length - 1; candidateIndex > 0; ) {
+		const candidate = fileSystem[candidateIndex];
+
+		if (candidate.id === -1) {
+			candidateIndex--;
+			continue;
+		}
+
+		const suitableIndex = fileSystem
+			.slice(0, candidateIndex)
+			.findIndex(
+				(fileOrEmptySpace) =>
+					fileOrEmptySpace.id === -1 &&
+					fileOrEmptySpace.length >= candidate.length,
+			);
+
+		if (suitableIndex < 0) {
+			candidateIndex--;
+			continue;
+		}
+
+		const currentSpace = fileSystem[suitableIndex];
+
+		if (currentSpace.length > candidate.length) {
+			const newSpaceRight = {
+				id: -1,
+				length: candidate.length,
+			};
+			const newSpaceLeft = {
+				id: -1,
+				length: currentSpace.length - candidate.length,
+			};
+
+			fileSystem = [
+				...fileSystem.slice(0, suitableIndex),
+				candidate,
+				newSpaceLeft,
+				...fileSystem.slice(suitableIndex + 1, candidateIndex),
+				newSpaceRight,
+				...fileSystem.slice(candidateIndex + 1),
+			];
+
+			// Because some of the space was left behind, and the candidate also moved
+			// left, there are now more elements of the left, so we don't decrement
+			// the index here. The next candidate will be at the same index in the
+			// next iteration as the current candidate this iteration
+
+			continue;
+		}
+
+		fileSystem = [
+			...fileSystem.slice(0, suitableIndex),
+			candidate,
+			...fileSystem.slice(suitableIndex + 1, candidateIndex),
+			currentSpace,
+			...fileSystem.slice(candidateIndex + 1),
+		];
+
+		candidateIndex--;
+	}
+
+	// calculate checksum
+	let checksum = 0;
+	let fileSystemBlockIndex = 0;
+
+	for (const fileOrEmptySpace of fileSystem) {
+		for (
+			let fileOrEmptySpaceBlockIndex = 0;
+			fileOrEmptySpaceBlockIndex < fileOrEmptySpace.length;
+			fileOrEmptySpaceBlockIndex++
+		) {
+			if (fileOrEmptySpace.id > 0) {
+				checksum += fileSystemBlockIndex * fileOrEmptySpace.id;
+			}
+
+			fileSystemBlockIndex++;
+		}
+	}
+
+	return checksum;
+}
+
+console.log(partTwo());
+
+export { partOne, partTwo };
