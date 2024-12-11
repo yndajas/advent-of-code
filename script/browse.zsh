3ff507a51ccdc980c0acfab18190392295399335
Ynda Jas <yndajas@gmail.com>
2024-12-11 11:25:16 +0000

    Simplify regexes in browse script
    Some of these were doing the same thing but with different tokens, so
    I've combined them with capture groups

--- script/browse.zsh
+++ script/browse.zsh
@@ -24,10 +24,8 @@ PROMPT_FILEPATH=$SCRIPT_FOLDER/../$YEAR/prompts/$ZERO_PADDED_DAY.html
 cat $PROMPT_FILEPATH \
   | npx html-minifier --collapse-whitespace --remove-tag-whitespace \
   | pandoc --from=html --to=markdown_strict+backtick_code_blocks --lua-filter=$SCRIPT_FOLDER/code_block.lua --wrap=none \
-  | sed 's/` \([.,:;!?)]\)/`\1/g' \
+  | sed 's/\([`*]\) \([.,:;!?)]\)/\1\2/g' \
   | sed 's/\([`*]\) \([-][^\s]\)/\1\2/g' \
-  | sed 's/* \([.,:;!?)]\)/*\1/g' \
-  | sed 's/\([(]\) `/\1`/g' \
+  | sed 's/\([(]\) \([`*]\)/\1\2/g' \
   | sed 's/\([^\s][-]\) \([`*]\)/\1\2/g' \
-  | sed 's/\([(]\) \*/\1*/g' \
   | glow -p -s $SCRIPT_FOLDER/glow.json
