d3da849ca00d7f8f98423f55ce1e06a3e076ffdc
Ynda Jas <yndajas@gmail.com>
2024-12-11 16:41:29 +0000

    Add 2024/11.2 TypeScript solution
    I tried using multi-threading with promises and `Promise.all` over each
    of the numbers, but that didn't make things fast enough at a depth of
    75. I guess this is an example of where the big O really matters, not
    the N, so dividing the N by 6 isn't going to help all that much
    
    After that, I tried a few things, and I knew I wanted to go depth first
    through blinks, memoising where each stone gets to at a given depth.
    I played around with a few loops, but I knew I should probably use
    recursion: without recursion, it would be a challenge to keep track of
    the blinks since every ancestor node when diving down. In the end I
    looked for some hints to help think through the structure of the
    recursive part, and while I was slowly getting somewhere close,
    `calculateStoneCountByBlinkCount` is broadly based on:
    https://www.reddit.com/r/adventofcode/comments/1hbo9m6/comment/m1i35km

--- 2024/solutions/typescript/11/solution.test.ts
+++ 2024/solutions/typescript/11/solution.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it } from "bun:test";
 
-import { partOne } from "./solution";
+import { partOne, partTwo } from "./solution";
 
 describe("2024 day 11", () => {
 	describe("part 1", () => {
 		it("returns 199986", async () => {
 			expect(partOne()).toEqual(199986);
+		});
+	});
+
+	describe("part 2", () => {
+		it("returns 236804088748754", () => {
+			expect(partTwo()).toEqual(236804088748754);
 		});
 	});
 });
--- 2024/solutions/typescript/11/solution.ts
+++ 2024/solutions/typescript/11/solution.ts
@@ -3,47 +3,88 @@ const stones = (await Bun.file(`${import.meta.dir}/../../../input/11`).text())
 	.map((stoneString) => Number.parseInt(stoneString, 10));
 
 function partOne() {
-	let mutableStones = [...stones];
-
-	for (let blinkCount = 0; blinkCount < 25; blinkCount++) {
-		mutableStones = blink(mutableStones);
-	}
-
-	return mutableStones.length;
+	return stoneCountByBlinkCount(25);
 }
 
-function blink(stones: number[]) {
-	const updatedStones: number[] = [];
+function partTwo() {
+	return stoneCountByBlinkCount(75);
+}
+
+function stoneCountByBlinkCount(blinkCount: number) {
+	const stoneCountByBlinkCountByStone: Record<
+		number,
+		Record<number, number>
+	> = {};
 
 	for (const stone of stones) {
-		if (stone === 0) {
-			updatedStones.push(1);
-			continue;
-		}
-
-		const stringifiedStone = stone.toString();
-		const digitCount = stringifiedStone.length;
-
-		if (digitCount % 2 === 0) {
-			const leftSide = Number.parseInt(
-				stringifiedStone.slice(0, digitCount / 2),
-				10,
-			);
-			const rightSide = Number.parseInt(
-				stringifiedStone.slice(digitCount / 2),
-				10,
-			);
-
-			updatedStones.push(leftSide, rightSide);
-			continue;
-		}
-
-		updatedStones.push(stone * 2024);
+		calculateStoneCountByBlinkCount(
+			stone,
+			blinkCount,
+			stoneCountByBlinkCountByStone,
+		);
 	}
 
-	return updatedStones;
+	return stones.reduce(
+		(accumulator, stone) =>
+			accumulator + stoneCountByBlinkCountByStone[stone][blinkCount],
+		0,
+	);
+}
+
+function calculateStoneCountByBlinkCount(
+	stone: number,
+	blinkCount: number,
+	stoneCountByBlinkCountByStone: Record<number, Record<number, number>>,
+): number {
+	if (blinkCount === 0) {
+		return 1;
+	}
+
+	if (stoneCountByBlinkCountByStone[stone]?.[blinkCount]) {
+		return stoneCountByBlinkCountByStone[stone][blinkCount];
+	}
+
+	stoneCountByBlinkCountByStone[stone] ||= {};
+
+	const stoneCount = blink(stone)
+		.map((childStone) =>
+			calculateStoneCountByBlinkCount(
+				childStone,
+				blinkCount - 1,
+				stoneCountByBlinkCountByStone,
+			),
+		)
+		.reduce((accumulator, childBlinkCount) => accumulator + childBlinkCount, 0);
+
+	stoneCountByBlinkCountByStone[stone][blinkCount] = stoneCount;
+	return stoneCount;
+}
+
+function blink(stone: number) {
+	if (stone === 0) {
+		return [1];
+	}
+
+	const stringifiedStone = stone.toString();
+	const digitCount = stringifiedStone.length;
+
+	if (digitCount % 2 === 0) {
+		const leftSide = Number.parseInt(
+			stone.toString().slice(0, digitCount / 2),
+			10,
+		);
+		const rightSide = Number.parseInt(
+			stone.toString().slice(digitCount / 2),
+			10,
+		);
+
+		return [leftSide, rightSide];
+	}
+
+	return [stone * 2024];
 }
 
 console.log(partOne());
+console.log(partTwo());
 
-export { partOne };
+export { partOne, partTwo };
