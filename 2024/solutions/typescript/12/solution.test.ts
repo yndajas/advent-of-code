1797fbf2a343db48b0b230c4afae03d46d50c9b1
Ynda Jas <yndajas@gmail.com>
2024-12-12 19:05:19 +0000

    Add 2024/12.2 TypeScript solution
    I considered walking the perimeter and counting every turn until
    reaching the same coordinate with the same orientation, which would be
    a bit of a call back to day 6, but I decided to try something different
    and maybe a little more mathematical?
    
    For each coordinate, it tracks the sides that don't neighbour another
    plot in the region (using a Set). Then for each orientation and x combo,
    it then filters the tracked coordinate/orientation objects. It takes
    their y values, sorts them, and counts the contiguous groups of
    integers. It then does the same with orientation and y combos, for each
    counting their contiguous y integer groups
    
    I think technically this has more steps than would the other approach,
    where you just count the rotations, going through a maximum of the
    perimeter cells * the directions. Here you go through each direction for
    each coordinate in the region, and then have to do the additional
    contiguous groups calculation at the end on each orientation for each
    row and each column. But regardless I kind of like this approach, and
    it takes around 65ms in my environment for this N count. Building a
    rotating walker/checker hybrid could easily result in a monster
    
    I almost had the right answer a few hours ago, but I hadn't realised or
    had forgotten that I need to give `sort` a compare function even if I
    just want a basic high to low or low to high sort... so when there were
    11+ rows or columns it didn't get the right number of contiguous groups
    because the numbers were improperly sorted. Of course none of the
    examples have that many rows or columns, so it worked perfectly on them

--- 2024/solutions/typescript/12/solution.test.ts
+++ 2024/solutions/typescript/12/solution.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it } from "bun:test";
 
-import { partOne } from "./solution";
+import { partOne, partTwo } from "./solution";
 
 describe("2024 day 12", () => {
 	describe("part 1", () => {
 		it("returns 1488414", () => {
 			expect(partOne()).toEqual(1488414);
+		});
+	});
+
+	describe("part 2", () => {
+		it("returns 911750", () => {
+			expect(partTwo()).toEqual(911750);
 		});
 	});
 });
--- 2024/solutions/typescript/12/solution.ts
+++ 2024/solutions/typescript/12/solution.ts
@@ -12,24 +12,13 @@ const cardinalMovements: CoordinateOrMovement[] = [
 ];
 
 type CoordinateOrMovement = { x: number; y: number };
+type Region = Set<CoordinateOrMovement>;
+
+type Orientation = "up" | "right" | "down" | "left";
+type PlotSide = CoordinateOrMovement & { orientation: Orientation };
 
 function partOne() {
-	const regions: Set<CoordinateOrMovement>[] = [];
-	const plotsRecorded = new Set<string>();
-
-	map.forEach((row, y) => {
-		row.split("").forEach((plotPlantType, x) => {
-			const coordinate = { x, y };
-
-			if (plotsRecorded.has(coordinateToString(coordinate))) {
-				return;
-			}
-
-			const region = new Set<CoordinateOrMovement>();
-			plotRegionFromCoordinate(region, plotsRecorded, coordinate);
-			regions.push(region);
-		});
-	});
+	const regions = getRegions();
 
 	return regions.reduce(
 		(accumulator, region) => accumulator + regionFencingPrice(region),
@@ -37,14 +26,46 @@ function partOne() {
 	);
 }
 
+function partTwo() {
+	const regions = getRegions();
+
+	return regions.reduce(
+		(accumulator, region) =>
+			accumulator + regionFencingPriceWithBulkDiscount(region),
+		0,
+	);
+}
+
 console.log(partOne());
+console.log(partTwo());
+
+function getRegions() {
+	const regions: Region[] = [];
+	const plotsRecorded = new Set<string>();
+
+	for (let y = 0; y <= maxY; y++) {
+		for (let x = 0; x <= maxX; x++) {
+			const coordinate = { x, y };
+
+			if (plotsRecorded.has(coordinateToString(coordinate))) {
+				continue;
+			}
+
+			const region: Region = new Set();
+			plotRegionFromCoordinate(region, plotsRecorded, coordinate);
+			regions.push(region);
+		}
+	}
+
+	return regions;
+}
 
 function coordinateToString(coordinate: CoordinateOrMovement) {
 	return `${coordinate.x}.${coordinate.y}`;
 }
 
 function plotRegionFromCoordinate(
-	region: Set<CoordinateOrMovement>,
+	region: Region,
 	plotsRecorded: Set<string>,
 	coordinate: CoordinateOrMovement,
 	targetPlantType: string | undefined = undefined,
@@ -99,15 +120,15 @@ function validPoint(coordinate: CoordinateOrMovement) {
 	);
 }
 
-function regionFencingPrice(region: Set<CoordinateOrMovement>) {
+function regionFencingPrice(region: Region) {
 	return regionArea(region) * regionPerimeter(region);
 }
 
-function regionArea(region: Set<CoordinateOrMovement>) {
+function regionArea(region: Region) {
 	return region.size;
 }
 
-function regionPerimeter(region: Set<CoordinateOrMovement>) {
+function regionPerimeter(region: Region) {
 	const regionArray = Array.from(region);
 	const plantType = map[regionArray[0].y][regionArray[0].x];
 
@@ -131,4 +152,111 @@ function regionPerimeter(region: Set<CoordinateOrMovement>) {
 	}, 0);
 }
 
-export { partOne };
+function regionFencingPriceWithBulkDiscount(region: Region) {
+	return regionArea(region) * regionSideCount(region);
+}
+
+function regionSideCount(region: Region) {
+	const plotSidesOnPerimeter = regionPlotSidesOnPerimeter(region);
+	let count = 0;
+
+	for (const orientation of <Orientation[]>["left", "right"]) {
+		for (let x = 0; x <= maxX; x++) {
+			const matchingPlotSides = plotSidesOnPerimeter.filter(
+				(side) => side.orientation === orientation && side.x === x,
+			);
+
+			count += contiguousGroupCount(
+				matchingPlotSides.map((side) => side.y).sort((a, b) => a - b),
+			);
+		}
+	}
+
+	for (const orientation of <Orientation[]>["up", "down"]) {
+		for (let y = 0; y <= maxY; y++) {
+			const matchingPlotSides = plotSidesOnPerimeter.filter(
+				(side) => side.orientation === orientation && side.y === y,
+			);
+
+			count += contiguousGroupCount(
+				matchingPlotSides.map((side) => side.x).sort((a, b) => a - b),
+			);
+		}
+	}
+
+	return count;
+}
+
+function regionPlotSidesOnPerimeter(region: Region) {
+	const regionStringSet = regionToStringSet(region);
+	const sides: PlotSide[] = [];
+
+	for (const coordinate of Array.from(region)) {
+		for (const cardinalMovement of cardinalMovements) {
+			const neighbourCoordinate = move(coordinate, cardinalMovement);
+
+			if (
+				!neighbourCoordinate ||
+				!regionStringSet.has(coordinateToString(neighbourCoordinate))
+			) {
+				sides.push({
+					...coordinate,
+					orientation: cardinalMovementToOrientation(cardinalMovement),
+				});
+			}
+		}
+	}
+
+	return sides;
+}
+
+function cardinalMovementToOrientation(
+	cardinalMovement: CoordinateOrMovement,
+): Orientation {
+	if (cardinalMovement.x === 0 && cardinalMovement.y === -1) {
+		return "up";
+	}
+
+	if (cardinalMovement.x === 1 && cardinalMovement.y === 0) {
+		return "right";
+	}
+
+	if (cardinalMovement.x === 0 && cardinalMovement.y === 1) {
+		return "down";
+	}
+
+	return "left";
+}
+
+function regionToStringSet(region: Region) {
+	return new Set(
+		Array.from(region).map((coordinate) => coordinateToString(coordinate)),
+	);
+}
+
+function contiguousGroupCount(ascendingIntegers: number[]) {
+	if (ascendingIntegers.length === 0) {
+		return 0;
+	}
+
+	let groupCount = 1;
+	let previous = ascendingIntegers[0];
+
+	for (
+		let numberIndex = 1;
+		numberIndex < ascendingIntegers.length;
+		numberIndex++
+	) {
+		const current = ascendingIntegers[numberIndex];
+
+		if (current !== previous + 1) {
+			groupCount++;
+		}
+
+		previous = current;
+	}
+
+	return groupCount;
+}
+
+export { partOne, partTwo };
