4fd83ea72c8bb6beba53880823d8996a665ea35a
Ynda Jas <yndajas@gmail.com>
2024-12-13 16:21:23 +0000

    Add 2024/13.2 TypeScript solution
    This also includes a divide and conquer version of part one and renames
    the original solution to make it clear it's brute force
    
    It's clear that there's only one good combination in each machine. I
    thought that the data were handcrafted to ensure that, but I think it's
    probably because for every a and b pair, if a has a higher x, b has a
    higher y, so naturally there can only be one magic midpoint before one
    side diverges
    
    Working out the proximity calculation and then the divide and conquer
    algorithm was a challenge here. And I spent a little time confused about
    why I was getting a fractional count in the divide and conquer approach,
    but I'd just forgotten to do a `% 1` check on counts with a proximity of
    1

--- 2024/solutions/typescript/13/solution.test.ts
+++ 2024/solutions/typescript/13/solution.test.ts
@@ -1,17 +1,34 @@
 import { describe, expect, it } from "bun:test";
 
-import { partOne, partOneWithDivision } from "./solution";
+import {
+	partOneBruteForce,
+	partOneWithDivideAndConquer,
+	partOneWithDivision,
+	partTwo,
+} from "./solution";
 
 describe("2024 day 13", () => {
-	describe("part 1", () => {
+	describe("part 1 brute force", () => {
 		it("returns 27157", () => {
-			expect(partOne()).toEqual(27157);
+			expect(partOneBruteForce()).toEqual(27157);
 		});
 	});
 
 	describe("part 1 with division", () => {
 		it("returns 27157", () => {
 			expect(partOneWithDivision()).toEqual(27157);
+		});
+	});
+
+	describe("part 1 with divide and conquer", () => {
+		it("returns 27157", () => {
+			expect(partOneWithDivideAndConquer()).toEqual(27157);
+		});
+	});
+
+	describe("part 2", () => {
+		it("returns 104015411578548", () => {
+			expect(partTwo()).toEqual(104015411578548);
 		});
 	});
 });
--- 2024/solutions/typescript/13/solution.ts
+++ 2024/solutions/typescript/13/solution.ts
@@ -3,35 +3,61 @@ const input = await Bun.file(`${import.meta.dir}/../../../input/13`).text();
 type CoordinateOrMovement = { x: number; y: number };
 type Button = CoordinateOrMovement & { cost: number };
 
-function partOne() {
+function partOneBruteForce() {
 	const machines = parseMachines();
 	return machines.reduce(
-		(accumulator, machine) => accumulator + machine.cheapestOptionPartOne(),
+		(accumulator, machine) => accumulator + machine.cheapestOptionBruteForce(),
 		0,
 	);
 }
 
 function partOneWithDivision() {
 	const machines = parseMachines();
-	let totalCost = 0;
 
-	for (const machine of machines) {
-		totalCost += machine.cheapestOptionWithDivision();
-	}
-
-	return totalCost;
+	return machines.reduce(
+		(accumulator, machine) =>
+			accumulator + machine.cheapestOptionWithDivision(),
+		0,
+	);
 }
 
-console.log(partOne());
-console.log(partOneWithDivision());
+function partOneWithDivideAndConquer() {
+	const machines = parseMachines();
+	return machines.reduce(
+		(accumulator, machine) =>
+			accumulator + machine.cheapestOptionWithDivideAndConquer(),
+		0,
+	);
+}
 
-function parseMachines() {
+function partTwo() {
+	const machines = parseMachines(10000000000000);
+	return machines.reduce(
+		(accumulator, machine) =>
+			accumulator + machine.cheapestOptionWithDivideAndConquer(),
+		0,
+	);
+}
+
+// console.log(partOneBruteForce());
+// console.log(partOneWithDivision());
+// console.log(partOneWithDivideAndConquer());
+// console.log(partTwo());
+
+function parseMachines(targetModifier = 0) {
 	return input.split("\n\n").map((machineString) => {
 		const [aX, aY, bX, bY, targetX, targetY] = machineString
 			.match(/\d+/g)
 			.map((match) => Number.parseInt(match, 10));
 
-		return new Machine({ aX, aY, bX, bY, targetX, targetY });
+		return new Machine({
+			aX,
+			aY,
+			bX,
+			bY,
+			targetX: targetX + targetModifier,
+			targetY: targetY + targetModifier,
+		});
 	});
 }
 
@@ -60,7 +86,7 @@ class Machine {
 		this.target = { x: targetX, y: targetY };
 	}
 
-	cheapestOptionPartOne() {
+	public cheapestOptionBruteForce() {
 		let cost = 0;
 		let aCount = 0;
 
@@ -94,7 +120,7 @@ class Machine {
 		return cost;
 	}
 
-	cheapestOptionWithDivision() {
+	public cheapestOptionWithDivision() {
 		const maxAX = Math.floor(this.target.x / this.a.x);
 		const maxAY = Math.floor(this.target.y / this.a.y);
 		const maxA = Math.min(maxAX, maxAY);
@@ -125,9 +151,73 @@ class Machine {
 		return cost;
 	}
 
-	overTarget(current: { x: number; y: number }) {
+	public cheapestOptionWithDivideAndConquer() {
+		const maxAX = Math.floor(this.target.x / this.a.x);
+		const maxAY = Math.floor(this.target.y / this.a.y);
+		let maxA = Math.min(maxAX, maxAY);
+		let minA = 0;
+		let cost = 0;
+
+		while (!cost) {
+			const aCount = this.midPoint(minA, maxA);
+			const proximity = this.proximity(aCount);
+
+			if (proximity === -1) {
+				break;
+			}
+
+			if (proximity === 1) {
+				const bCount = this.remainder(aCount).x / this.b.x;
+
+				// if the proximity is 1 when the count required is not an integer, the
+				// machine isn't winnable
+				if (bCount % 1 !== 0) {
+					break;
+				}
+
+				cost = this.a.cost * aCount + this.b.cost * bCount;
+				break;
+			}
+
+			if (aCount > minA && this.proximity(aCount - 1) < proximity) {
+				maxA = aCount - 1;
+			} else if (aCount < maxA && this.proximity(aCount + 1) < proximity) {
+				minA = aCount + 1;
+			} else break;
+		}
+
+		return cost;
+	}
+
+	private midPoint(min: number, max: number) {
+		return min + Math.floor((max - min) / 2);
+	}
+
+	private proximity(aCount: number) {
+		const remainder = this.remainder(aCount);
+		const bsNeededX = remainder.x / this.b.x;
+		const bsNeededY = remainder.y / this.b.y;
+
+		return bsNeededX > bsNeededY
+			? bsNeededX / bsNeededY
+			: bsNeededY / bsNeededX;
+	}
+
+	private remainder(aCount) {
+		return {
+			x: this.target.x - this.a.x * aCount,
+			y: this.target.y - this.a.y * aCount,
+		};
+	}
+
+	private overTarget(current: { x: number; y: number }) {
 		return current.x > this.target.x || current.y > this.target.y;
 	}
 }
 
-export { partOne, partOneWithDivision };
+export {
+	partOneBruteForce,
+	partOneWithDivision,
+	partOneWithDivideAndConquer,
+	partTwo,
+};
