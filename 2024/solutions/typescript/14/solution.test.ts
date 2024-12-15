4732553055ef03941fe8897dc1e9aa91e55b1014
Ynda Jas <yndajas@gmail.com>
2024-12-15 03:55:09 +0000

    Add 2024/14.2 TypeScript solution
    I'm not particularly fond of these solutions. They feel like exploits
    based on characteristics of the data rather than solid algorithms. I
    don't think there's any particular reason that these work other than the
    data happening to be generated in a particular way
    
    I didn't really get the prompt at first, so I hopped over to Reddit to
    see if there were any threads explaining the problem. I stumbled on a
    few things:
    - printed maps showing what the tree should look like
    - mention that there are no overlapping robots during the easter egg
      second
    - mention that the easter egg second might be the only one with a
      certain number of a adjacent robots (in a line or a square or a
      contiguous region)
    - mention of noticing that a near Christmas tree appeared at certain
      intervals, and the complete tree was on this interval
    - mention that the configuration of robots must repeat every width *
      length seconds, since the robots loop around. Their x positions will
      loop every width seconds, and their y positions every height seconds,
      so to get them both to loop you need to go at most width * height
      seconds
    
    Te second and third points feel like quirks of the data, though I
    suppose you could intuit that looking for a substantial number of
    adjacent robots would be useful
    
    I implemented a couple of solutions along those lines. My basic
    implementation of the adjacency check is pretty slow, but having seen
    the interval hint and example trees, I had a think about optimisations.
    I decided to go through a whole loop of permutations (width * height
    seconds as mentioned above) recording the most robots in a column or row
    for each, then check which second had the most. It turns out that the
    second with the easter egg is a multiple of this second (or its index?).
    The maximum robots in a line check was less expensive than the adjacency
    check (in my implementation), so I did the former and then checked every
    second that was a multiple of that second, which cut the time down to
    close to (but still slightly slower than) the overlap checking solution
    
    The fact that the second with the most robots in a single line is a
    factor/divisor of the easter egg second - rather than another multiple
    of a shared factor/divisor, for instance, or just completely unrelated
    - again feels like a quirk of the data. Likely intentional, or an
    artefact of the data generation method, but I'm not sure how you're
    supposed to find this pattern without lucky guesswork

--- 2024/solutions/typescript/14/solution.test.ts
+++ 2024/solutions/typescript/14/solution.test.ts
@@ -1,11 +1,38 @@
 import { describe, expect, it } from "bun:test";
 
-import { partOne } from "./solution";
+import {
+	partOne,
+	partTwoFindLine,
+	partTwoFindLineButWithBiggerIncrements,
+	partTwoLookForLackOfOverlaps,
+} from "./solution";
 
 describe("2024 day 14", () => {
 	describe("part 1", () => {
 		it("returns 218619120", () => {
 			expect(partOne({ width: 101, height: 103 })).toEqual(218619120);
+		});
+	});
+
+	describe("part 2 by looking for a lack of overlapping robots", () => {
+		it("returns 7055", () => {
+			expect(partTwoLookForLackOfOverlaps({ width: 101, height: 103 })).toEqual(
+				7055,
+			);
+		});
+	});
+
+	describe("part 2 by finding a line of adjacent robots over a manually-found threshold", () => {
+		it("returns 7055", () => {
+			expect(partTwoFindLine({ width: 101, height: 103 })).toEqual(7055);
+		});
+	});
+
+	describe("part 2 by finding a line of adjacent robots over a manually-found threshold, but incrementing the seconds checked by the index of the second with the most robots in a single line", () => {
+		it("returns 7055", () => {
+			expect(
+				partTwoFindLineButWithBiggerIncrements({ width: 101, height: 103 }),
+			).toEqual(7055);
 		});
 	});
 });
--- 2024/solutions/typescript/14/solution.ts
+++ 2024/solutions/typescript/14/solution.ts
@@ -20,6 +20,66 @@ function partOne(space: Space) {
 	);
 }
 
+function partTwoLookForLackOfOverlaps(space: Space) {
+	let robots = linesToRobots();
+	let seconds = 0;
+
+	while (hasOverlaps(robots)) {
+		robots = moveRobots(robots, space, 1);
+		seconds++;
+	}
+
+	return seconds;
+}
+
+function partTwoFindLine(space: Space) {
+	let robots = linesToRobots();
+	let seconds = 0;
+
+	while (!hasLineOfRobots(robots, space)) {
+		robots = moveRobots(robots, space, 1);
+		seconds++;
+	}
+
+	return seconds;
+}
+
+function partTwoFindLineButWithBiggerIncrements(space: Space) {
+	let robots = linesToRobots();
+	const maxRobotsInASingleLineEachSecond: number[] = [];
+
+	for (let seconds = 0; seconds <= space.width * space.height; seconds++) {
+		robots = moveRobots(robots, space, 1);
+		maxRobotsInASingleLineEachSecond.push(
+			maxRobotsInASingleLine(robots, space),
+		);
+	}
+
+	const indexOfSecondWithMaxRobotsOnASingleLine =
+		maxRobotsInASingleLineEachSecond.indexOf(
+			Math.max(...maxRobotsInASingleLineEachSecond),
+		);
+
+	robots = linesToRobots();
+	robots = moveRobots(robots, space, indexOfSecondWithMaxRobotsOnASingleLine);
+	let seconds = indexOfSecondWithMaxRobotsOnASingleLine;
+
+	while (!hasLineOfRobots(robots, space)) {
+		robots = moveRobots(robots, space, indexOfSecondWithMaxRobotsOnASingleLine);
+		seconds += indexOfSecondWithMaxRobotsOnASingleLine;
+	}
+
+	return seconds;
+}
+
+// console.log("part one example data:", partOne({ width: 11, height: 7 }));
+// console.log(partOne({ width: 101, height: 103 }));
+// console.log(partTwoLookForLackOfOverlaps({ width: 101, height: 103 }));
+// console.log(partTwoFindLine({ width: 101, height: 103 }));
+// console.log(
+// 	partTwoFindLineButWithBiggerIncrements({ width: 101, height: 103 }),
+// );
+
 function linesToRobots(): Robot[] {
 	return lines.map((line) => {
 		const [startX, startY, moveX, moveY] = line
@@ -86,9 +146,90 @@ function moveRobots(robots: Robot[], space: Space, seconds: number): Robot[] {
 	});
 }
 
-// example input
-console.log("part one example data:", partOne({ width: 11, height: 7 }));
-// actual input
-console.log("part one actual data:", partOne({ width: 101, height: 103 }));
+function hasOverlaps(robots: Robot[]) {
+	const robotCoordinates: Set<string> = new Set();
 
-export { partOne };
+	return robots.some((robot) => {
+		const coordinateString = coordinateToString(robot.coordinate);
+
+		if (robotCoordinates.has(coordinateString)) return true;
+
+		robotCoordinates.add(coordinateString);
+
+		return false;
+	});
+}
+
+function coordinateToString(coordinate: CoordinateOrMovement) {
+	return `${coordinate.x}.${coordinate.y}`;
+}
+
+function hasLineOfRobots(robots: Robot[], space: Space, minimumLength = 8) {
+	const coordinatesWithRobots: Set<string> = new Set();
+
+	for (const robot of robots) {
+		coordinatesWithRobots.add(coordinateToString(robot.coordinate));
+	}
+
+	let run = 0;
+
+	for (let y = 0; y < space.height; y++) {
+		for (let x = 0; x < space.width; x++) {
+			if (coordinatesWithRobots.has(coordinateToString({ x, y }))) {
+				run++;
+			} else {
+				run = 0;
+			}
+
+			if (run === minimumLength) {
+				return true;
+			}
+		}
+
+		run = 0;
+	}
+
+	return false;
+}
+
+function maxRobotsInASingleLine(robots: Robot[], space: Space) {
+	const robotsPerX = new Array(space.width);
+	robotsPerX.fill(0);
+	const robotsPerY = new Array(space.height);
+	robotsPerY.fill(0);
+
+	for (const robot of robots) {
+		robotsPerX[robot.coordinate.x]++;
+		robotsPerY[robot.coordinate.y]++;
+	}
+
+	return Math.max(Math.max(...robotsPerX), Math.max(...robotsPerY));
+}
+
+function printRobots(robots: Robot[], space) {
+	const robotCountByCoordinate: Record<string, number> = {};
+
+	for (const robot of robots) {
+		robotCountByCoordinate[coordinateToString(robot.coordinate)] =
+			(robotCountByCoordinate[coordinateToString(robot.coordinate)] || 0) + 1;
+	}
+
+	let map = "";
+
+	for (let y = 0; y < space.height; y++) {
+		for (let x = 0; x < space.width; x++) {
+			map += robotCountByCoordinate[coordinateToString({ x, y })] || ".";
+		}
+
+		map += "\n";
+	}
+
+	console.log(map);
+}
+
+export {
+	partOne,
+	partTwoLookForLackOfOverlaps,
+	partTwoFindLine,
+	partTwoFindLineButWithBiggerIncrements,
+};
