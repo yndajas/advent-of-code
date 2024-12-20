1258f05df646f813599276d00adf0ac9166a4af2
Ynda Jas <yndajas@gmail.com>
2024-12-20 00:42:17 +0000

    Add 2024/18.1 TypeScript solution
    This is a little slow (4-5s). I probably need to learn Dijkstra's
    algorithm to optimise solutions to these kinds of problems

---
+++ 2024/solutions/typescript/18/solution.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "bun:test";
+import { partOne } from "./solution";
+
+describe("2024 day 18", () => {
+	describe("part 1", () => {
+		it("returns 446", () => {
+			expect(partOne()).toEqual(446);
+		});
+	});
+});
---
+++ 2024/solutions/typescript/18/solution.ts
@@ -0,0 +1,200 @@
+type CoordinateOrMovement = { x: number; y: number };
+
+const byteFallCoordinates: CoordinateOrMovement[] = (
+	await Bun.file(`${import.meta.dir}/../../../input/18`).text()
+)
+	.split("\n")
+	.map((coordinateString) => {
+		const [x, y] = coordinateString
+			.split(",")
+			.map((coordinatePartString) => Number.parseInt(coordinatePartString, 10));
+
+		return { x, y };
+	});
+
+type MapArray = string[][];
+type Path = CoordinateOrMovement[];
+type LeastStepsByCoordinate = Record<string, number>;
+
+function partOne(rowAndColumnCount = 71, fallenByteCount = 1024) {
+	const map: MapArray = generateMap(rowAndColumnCount);
+
+	markFallenBytes(map, fallenByteCount);
+
+	return leastSteps(
+		map,
+		{ x: 0, y: 0 },
+		{ x: rowAndColumnCount - 1, y: rowAndColumnCount - 1 },
+	);
+}
+
+console.log(partOne());
+// console.log(partOne(7, 12));
+
+function generateMap(rowAndColumnCount: number) {
+	const map: MapArray = [];
+
+	for (let y = 0; y < rowAndColumnCount; y++) {
+		map[y] = [];
+		for (let x = 0; x < rowAndColumnCount; x++) {
+			map[y][x] = ".";
+		}
+	}
+
+	return map;
+}
+
+function markFallenBytes(map: MapArray, fallenByteCount: number) {
+	for (const coordinate of byteFallCoordinates.slice(0, fallenByteCount)) {
+		const { x, y } = coordinate;
+
+		map[y][x] = "#";
+	}
+}
+
+function leastSteps(
+	map: MapArray,
+	from: CoordinateOrMovement,
+	to: CoordinateOrMovement,
+) {
+	let best: number;
+	const currentPath: Path = [{ ...from }];
+	const leastStepsByCoordinate: Record<string, number> = {};
+
+	while (currentPath.length > 0) {
+		if (best && best <= stepCount(currentPath) + 1) {
+			currentPath.pop();
+		} else if (
+			moveSouth(map, currentPath, leastStepsByCoordinate) ||
+			moveEast(map, currentPath, leastStepsByCoordinate) ||
+			moveNorth(map, currentPath, leastStepsByCoordinate) ||
+			moveWest(map, currentPath, leastStepsByCoordinate)
+		) {
+			if (coordinatesMatch(currentCoordinateFromPath(currentPath), to)) {
+				best = stepCount(currentPath);
+
+				currentPath.pop();
+			}
+		} else {
+			currentPath.pop();
+		}
+	}
+
+	return best;
+}
+
+function currentCoordinateFromPath(path: Path) {
+	return path[path.length - 1];
+}
+
+function coordinatesMatch(a: CoordinateOrMovement, b: CoordinateOrMovement) {
+	return a.x === b.x && a.y === b.y;
+}
+
+function coordinateToString({ x, y }: CoordinateOrMovement) {
+	return `${x},${y}`;
+}
+
+function moveSouth(
+	map: MapArray,
+	currentPath: Path,
+	leastStepsByCoordinate: LeastStepsByCoordinate,
+) {
+	const currentCoordinate = currentCoordinateFromPath(currentPath);
+	const nextCoordinate = { ...currentCoordinate, y: currentCoordinate.y + 1 };
+
+	return move(map, nextCoordinate, currentPath, leastStepsByCoordinate);
+}
+
+function moveEast(
+	map: MapArray,
+	currentPath: Path,
+	leastStepsByCoordinate: LeastStepsByCoordinate,
+) {
+	const currentCoordinate = currentCoordinateFromPath(currentPath);
+	const nextCoordinate = { ...currentCoordinate, x: currentCoordinate.x + 1 };
+
+	return move(map, nextCoordinate, currentPath, leastStepsByCoordinate);
+}
+
+function moveNorth(
+	map: MapArray,
+	currentPath: Path,
+	leastStepsByCoordinate: LeastStepsByCoordinate,
+) {
+	const currentCoordinate = currentCoordinateFromPath(currentPath);
+	const nextCoordinate = { ...currentCoordinate, y: currentCoordinate.y - 1 };
+
+	return move(map, nextCoordinate, currentPath, leastStepsByCoordinate);
+}
+
+function moveWest(
+	map: MapArray,
+	currentPath: Path,
+	leastStepsByCoordinate: LeastStepsByCoordinate,
+) {
+	const currentCoordinate = currentCoordinateFromPath(currentPath);
+	const nextCoordinate = { ...currentCoordinate, x: currentCoordinate.x - 1 };
+
+	return move(map, nextCoordinate, currentPath, leastStepsByCoordinate);
+}
+
+function move(
+	map: MapArray,
+	nextCoordinate: CoordinateOrMovement,
+	currentPath: Path,
+	leastStepsByCoordinate: LeastStepsByCoordinate,
+) {
+	if (outOfBounds(map, nextCoordinate)) return false;
+	if (fallenByteAtCoordinate(map, nextCoordinate)) return false;
+	if (pathIncludesCoordinate(currentPath, nextCoordinate)) return false;
+
+	const nextCoordinateString = coordinateToString(nextCoordinate);
+
+	if (
+		leastStepsByCoordinate[nextCoordinateString] <=
+		stepCount(currentPath) + 1
+	)
+		return false;
+
+	currentPath.push({ ...nextCoordinate });
+	leastStepsByCoordinate[nextCoordinateString] = stepCount(currentPath);
+
+	return true;
+}
+
+function stepCount(path: Path) {
+	return path.length - 1;
+}
+
+function fallenByteAtCoordinate(map: MapArray, { x, y }: CoordinateOrMovement) {
+	return map[y][x] === "#";
+}
+
+function outOfBounds(map: MapArray, { x, y }: CoordinateOrMovement) {
+	if (x < 0 || y < 0) return true;
+
+	return y >= map.length || x >= map[0].length;
+}
+
+function pathIncludesCoordinate(path: Path, coordinate: CoordinateOrMovement) {
+	return path.some((coordinateInPath) =>
+		coordinatesMatch(coordinateInPath, coordinate),
+	);
+}
+
+function printMap(map: MapArray) {
+	let mapString = "";
+
+	for (let y = 0; y < map.length; y++) {
+		for (let x = 0; x < map[0].length; x++) {
+			mapString += map[y][x];
+		}
+
+		mapString += "\n";
+	}
+
+	console.log(mapString);
+}
+
+export { partOne };
