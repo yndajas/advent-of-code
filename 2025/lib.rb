1b11e03e1595e3cb8c2e36098d304c2e15305f7f
Ynda Jas <yndajas@gmail.com>
2025-12-02 00:45:23 +0000

    Add Ruby helpers for 2025
    A few simple helpers for input retrieval and testing and benchmarking
    solutions

---
+++ 2025/lib.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'benchmark'
+
+def get_input(file)
+  File.readlines("2025/input/#{file}").map(&:chomp)
+end
+
+def test(description, actual:, expected: nil)
+  result = case expected
+  when nil
+    '❓'
+  when actual
+    '✅'
+  else
+    '❌'
+  end
+
+  output = "#{description} #{result}"
+
+  if expected.nil?
+    output += "\ngot: #{actual.inspect}\n\n"
+  elsif expected && actual != expected
+    output += "\nexpected: #{expected.inspect}\ngot: #{actual.inspect}\n\n"
+  end
+
+  puts output
+end
+
+def benchmark(description, times: [1, 10, 100], &block)
+  times.each do |count|
+    puts "#{description} x #{count}"
+    puts Benchmark.measure { count.times { block.call } }
+  end
+end
