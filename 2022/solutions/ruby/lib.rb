d0198fead40b2a44624a85aa106dc7c1fcc448af
Ynda Jas <yndajas@gmail.com>
2026-07-13 23:18:54 +0100

    Copy 2025 Ruby lib to 2022
    It would be nice to make this shared, but for now I'm doing the easy
    thing

---
+++ 2022/solutions/ruby/lib.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'benchmark'
+require 'dotenv'
+
+Dotenv.load
+
+def get_input(file, raw: false)
+  assets_repo = ENV.fetch('ASSETS_REPO') { abort('ASSETS_REPO not set') }
+  path = "#{assets_repo}/2022/input/#{file}"
+
+  if raw
+    File.read(path)
+  else
+    File.readlines(path).map(&:chomp)
+  end
+end
+
+def test(description, actual:, expected: nil)
+  result = case expected
+  when nil
+    '❓'
+  when actual
+    '✅'
+  else
+    '❌'
+  end
+
+  output = "#{result} #{description}"
+
+  if expected.nil?
+    output += " - got #{actual.inspect}"
+  elsif expected && actual != expected
+    output += " - expected #{expected.inspect}, got #{actual.inspect}"
+  end
+
+  puts output
+end
+
+def test_multiple(test_args)
+  puts "\n#{Time.now.strftime('%F %T')}"
+
+  test_args.each { |desciption, named| test(desciption, **named) }
+end
+
+def benchmark(description: '', times: [1, 10, 100], &block)
+  times.each do |count|
+    puts "#{description} x #{count}".strip
+    puts(Benchmark.measure { count.times { block.call } })
+  end
+end
